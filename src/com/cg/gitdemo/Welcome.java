89d1515363b887339ee674d8fb17a910aa5c1260
K <ramesh.a.k@capgemini.com>
2022-06-15 10:23:23 +0530

    second commit

--- src/com/cg/gitdemo/Welcome.java
+++ src/com/cg/gitdemo/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 	
 	public static void main(String[] args) {
 		
-	
-	System.out.println("Welcome to Capgemini");
+	    System.out.println("Welcome to Capgemini");
+	    System.out.println("thank you");
 	}
 }
