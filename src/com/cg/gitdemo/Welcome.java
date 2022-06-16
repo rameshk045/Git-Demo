3b1ddd3f3a24e6b111b98684e031edf05c4c8496
K <ramesh.a.k@capgemini.com>
2022-06-16 16:42:13 +0530

    changes in third commit

--- src/com/cg/gitdemo/Welcome.java
+++ src/com/cg/gitdemo/Welcome.java
@@ -2,9 +2,9 @@ package com.cg.gitdemo;
 
 public class Welcome {
 	
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
+		
 	    System.out.println("Welcome to Capgemini");
 	    System.out.println("thank you");
-	    System.out.println("I am back");
 	}
 }
